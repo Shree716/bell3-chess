13c4e8acddb412c4a43df5502dfd95f658a92693
Shree716 <122548030+Shree716@users.noreply.github.com>
2023-03-01 11:25:25 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const Cursor = SpriteKind.create()
+}
 function createPiece (kind: string, black: boolean, image2: Image) {
     tempSprite = sprites.create(image2, SpriteKind.Player)
     sprites.setDataString(tempSprite, "kind", kind)
@@ -190,6 +193,64 @@ function setupBoard () {
         . a a a a a . . 
         `), tiles.getTileLocation(10, 10))
 }
+function can_i_move_here (asker: Sprite, col: number, row: number) {
+    if (col < 6 || col > 13) {
+        return false
+    } else if (row < 3 || row > 10) {
+        return false
+    }
+    for (let value of sprites.allOfKind(SpriteKind.Player)) {
+        if (value.tilemapLocation().column == col && value.tilemapLocation().row == row) {
+            if (sprites.readDataBoolean(value, "black") == sprites.readDataBoolean(asker, "black")) {
+                return true
+            }
+            break;
+        }
+    }
+    return true
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+    if (controller.A.isPressed()) {
+        highlight_move_spots(sprite)
+    }
+})
+function highlight_move_spots (piece: Sprite) {
+    if (sprites.readDataString(piece, "kind") == "pawn") {
+    	
+    } else if (sprites.readDataString(piece, "kind") == "knight") {
+    	
+    } else if (sprites.readDataString(piece, "kind") == "rook") {
+    	
+    } else if (sprites.readDataString(piece, "kind") == "bishop") {
+    	
+    } else if (sprites.readDataString(piece, "kind") == "king") {
+    	
+    } else if (sprites.readDataString(piece, "kind") == "bishop") {
+    	
+    }
+}
 let tempSprite: Sprite = null
 tiles.loadMap(tiles.createSmallMap(tilemap`level2`))
 setupBoard()
+let theCursor = sprites.create(img`
+    . . f f . . . . . . 
+    . f 1 b f f f f . . 
+    . f 1 1 f 1 f 1 f . 
+    . f b 1 1 b 1 b f . 
+    . . f b 1 1 1 1 1 f 
+    . f f f 1 1 1 1 1 f 
+    f 1 b f 1 1 1 1 1 f 
+    f 1 1 b 1 1 1 1 1 f 
+    . f 1 1 1 1 1 1 f . 
+    . . f f f f f f . . 
+    `, SpriteKind.Cursor)
+theCursor.setFlag(SpriteFlag.Ghost, true)
+controller.moveSprite(theCursor, 50, 50)
+let cursorTarget = sprites.create(img`
+    3 
+    `, SpriteKind.Cursor)
+cursorTarget.setFlag(SpriteFlag.Invisible, true)
+game.onUpdate(function () {
+    cursorTarget.top = theCursor.top
+    cursorTarget.top = theCursor.x - 2
+})
